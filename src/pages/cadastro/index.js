3600fd6156f208e64969747555d357ffdb7fe447
Rafael_97.Dev <33671644+Rafael97Sis@users.noreply.github.com>
2021-06-30 00:10:03 -0300

    update 8

--- src/pages/cadastro/index.js
+++ src/pages/cadastro/index.js
@@ -7,25 +7,25 @@ function Cadastro() {
 
   const baseURL = "http://localhost:3006/cadastra";
 
-  const [cadastro, setCadastro] = useState({ nome:'',email: '', definicao: '', cnpj: '', cpf: '', telefone: '', cep: '', endereco: '', nro: '', bairro: '', senha: '' })
+  const [cadastro, setCadastro] = useState({ nome: '', email: '', definicao: '', cnpj: '', cpf: '', telefone: '', cep: '', endereco: '', nro: '', bairro: '', senha: '' })
   //const [cadastroConta, setcadastroConta] = useState(cadastroContaState)
- 
+
   const handleInputChange = (event) => {
     const { name, value } = event.target
     setCadastro({ ...cadastro, [name]: value })
-    }
+  }
 
-    async function Cadastrar(cadastro) {
-      // const response = await
-       fetch (baseURL, {
-        method: "POST",
-        headers:{
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        },
-        body: JSON.stringify(cadastro)
-      });
-    }
+  async function Cadastrar(cadastro) {
+    // const response = await
+    fetch(baseURL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(cadastro)
+    });
+  }
 
   return (
 
@@ -36,12 +36,12 @@ function Cadastro() {
         <h1 style={{ color: '#9346F4', fontSize: '25px' }}> Crie sua conta e comece já! </h1>
 
         <form onSubmit={(event) => {
-            event.preventDefault();
-            //if(!cadastro.nome || !cadastro.email || !cadastro.definicao)return;
-            Cadastrar(cadastro)
-            setCadastro({nome:'',email: '', definicao: '', cnpj: '', cpf: '', telefone: '', cep: '', endereco: '', nro: '', bairro: '', senha: ''})
-            alert("cadastro ok ")
-          }}
+          event.preventDefault();
+          //if(!cadastro.nome || !cadastro.email || !cadastro.definicao)return;
+          Cadastrar(cadastro)
+          setCadastro({ nome: '', email: '', definicao: '', cnpj: '', cpf: '', telefone: '', cep: '', endereco: '', nro: '', bairro: '', senha: '' })
+          alert("cadastro ok ")
+        }}
         >
 
           <input
@@ -54,88 +54,85 @@ function Cadastro() {
           />
 
 
-          <input 
-          className='form-dados-cadastro-1'
-          type='number'
-          name='cep' 
-          value={cadastro.cep}
-          onChange={handleInputChange}
-          placeholder='CEP:' 
+          <input
+            className='form-dados-cadastro-1'
+            type='number'
+            name='cep'
+            value={cadastro.cep}
+            onChange={handleInputChange}
+            placeholder='CEP:'
           />
 
-          <input 
-          className='form-dados-cadastro-1'
-          name='email'
-          type='email' 
-          value={cadastro.email}
-          onChange={handleInputChange}
-          placeholder="Digite Email:" />
+          <input
+            className='form-dados-cadastro-1'
+            name='email'
+            type='email'
+            value={cadastro.email}
+            onChange={handleInputChange}
+            placeholder="Digite Email:" />
 
           <input
-           className='form-dados-cadastro-1'
-           name='endereco' 
-           type='text'
-           value={cadastro.endereco}
-           onChange={handleInputChange}
-           placeholder="Endereço:" />
+            className='form-dados-cadastro-1'
+            name='endereco'
+            type='text'
+            value={cadastro.endereco}
+            onChange={handleInputChange}
+            placeholder="Endereço:" />
 
-          <input 
-          className='form-dados-cadastro-1' 
-          name='cpf' 
-          type='number'
-          value={cadastro.cpf}
-          onChange={handleInputChange} 
-          placeholder="CNPJ ou CPF :" />
+          <input
+            className='form-dados-cadastro-1'
+            name='cpf'
+            type='number'
+            value={cadastro.cpf}
+            onChange={handleInputChange}
+            placeholder="CNPJ ou CPF :" />
 
-          <input 
-          className='form-dados-cadastro-1' 
-          type='number'
-          name='nro'
-          value={cadastro.nro}
-          onChange={handleInputChange}
-          placeholder='Nº ' />
+          <input
+            className='form-dados-cadastro-1'
+            type='number'
+            name='nro'
+            value={cadastro.nro}
+            onChange={handleInputChange}
+            placeholder='Nº ' />
 
-          <input 
-          className='form-dados-cadastro-1' 
-          type='number' 
-          name='telefone'
-          value={cadastro.telefone}
-          onChange={handleInputChange}
-          placeholder='Telefone:' />
+          <input
+            className='form-dados-cadastro-1'
+            type='number'
+            name='telefone'
+            value={cadastro.telefone}
+            onChange={handleInputChange}
+            placeholder='Telefone:' />
 
-          <input 
-          className='form-dados-cadastro-1' 
-          name='bairro' 
-          type='text' 
-          value={cadastro.bairro}
-          onChange={handleInputChange}
-          placeholder="Bairro:" />
+          <input
+            className='form-dados-cadastro-1'
+            name='bairro'
+            type='text'
+            value={cadastro.bairro}
+            onChange={handleInputChange}
+            placeholder="Bairro:" />
 
-          <input 
-          className='form-dados-cadastro-1' 
-          name='senha' 
-          type='password'
-          value={cadastro.senha}
-          onChange={handleInputChange}
-          placeholder="Senha:" />
+          <input
+            className='form-dados-cadastro-1'
+            name='senha'
+            type='password'
+            value={cadastro.senha}
+            onChange={handleInputChange}
+            placeholder="Senha:" />
 
           <input className='form-dados-cadastro-1' name='senha' type='password' placeholder="Confirmar Senha:" />
 
           <h2 style={{ color: '#9346F4', fontSize: '16px' }} > Vocé é um cliente ou profissional? </h2>
 
-          <select className="segmento">
-            <option 
-            name='cliente' 
+          <select 
+            name='definicao'
+            className="segmento"
             value={cadastro.definicao}
             onChange={handleInputChange}
-            >Cliente</option>
+          >
+            <option value="cliente"> Cliente </option>
 
-            <option 
-            name='profissional' 
-            value={cadastro.definicao}
-            onChange={handleInputChange}
-            >Profissional</option>
-            
+            <option value="profissional"> Profissional </option>
+
             <option value="perfil" selected> Selecione seu tipo de Perfil </option>
           </select >
 
@@ -144,8 +141,16 @@ function Cadastro() {
 
           <input className='form-checkbox' type="checkbox" id="profissional" name="profissional" value="Bike" />
           <label className='form-checkbox' for="profissional"> Profissional </label> */}
-          
+
           <h2 style={{ color: '#9346F4', fontSize: '16px' }} >  Tipo de Segmento? </h2>
+
+          <select className="segmento">
+            <option value="informatica">Informatica</option>
+            <option value="eletrica">Eletrica</option>
+            <option value="segmento" selected> Selecione seu tipo de Segmento</option>
+          </select>
+
+          <h2 style={{ color: '#9346F4', fontSize: '16px' }} >  Qual seu  tipo de Especialidade ? </h2>
 
           <select className="segmento">
             <option value="informatica">Informatica</option>
@@ -166,7 +171,7 @@ function Cadastro() {
 
 
   )
-  
+
 
 
 
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }  from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import NavBar from "../../components/navBar/navBar";
 import './index.css'
@@ -7,7 +7,7 @@ function Login() {
 
   const baseURL = "http://localhost:3006/cadastro/login";
 
-  const [usuario, setUsuario] = useState({email:'', senha:''})
+  const [usuario, setUsuario] = useState({ email: '', senha: '' })
   const handleInputChange = (event) => {
     const { name, value } = event.target
     setUsuario({ ...usuario, [name]: value })
@@ -30,35 +30,35 @@ function Login() {
 
         <form
           onSubmit={(event) => {
-          event.preventDefault()
-          logar(usuario)
+            event.preventDefault()
+            logar(usuario)
           }}
         >
 
 
           <input
-           className='form-dados-1'
-           name='email'
-           type='email'
-           value={usuario.email}
-           onChange={handleInputChange}
-           placeholder="Email:" />
+            className='form-dados-1'
+            name='email'
+            type='email'
+            value={usuario.email}
+            onChange={handleInputChange}
+            placeholder="Email:" />
 
 
-          <input 
-          className='form-dados-2' 
-          name='senha' 
-          type='password'
-          value={usuario.senha}
-          onChange={handleInputChange} 
-          placeholder="Senha:" />
+          <input
+            className='form-dados-2'
+            name='senha'
+            type='password'
+            value={usuario.senha}
+            onChange={handleInputChange}
+            placeholder="Senha:" />
 
           <button className='button-form-dados'> Logar </button>
 
-          
+
 
         </form>
-        <p className='box-senha'>Esqueceu sua senha?<Link style={{color:' #9346F4', fontSize:'18px'}} to='/'>Clique aqui </Link> </p>
+        <p className='box-senha'>Esqueceu sua senha?<Link style={{ color: ' #9346F4', fontSize: '18px' }} to='/'>Clique aqui </Link> </p>
       </div>
 
 
@@ -69,24 +69,29 @@ function Login() {
 
   async function logar(usuario) {
     const response = await fetch(baseURL, {
-        method:"POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        },
-        body: JSON.stringify(usuario)
-      });
-      if(response.ok) {
-        const data = await response.json();
-        sessionStorage.setItem("access-token", data.token);
-        window.location.href = '/';
-
-      } else if(response.status === 401) {
-        window.location.href = '/unauthorized';
-      } else {
-        const data = await response.json();
-        window.alert("Ops, algum problema ocorreu!");
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(usuario)
+    });
+    if (response.ok) {
+      const data = await response.json();
+      sessionStorage.setItem("access-token", data.token);
+      
+      if (data.definicao === "profissional") {
+        window.location.href = '/dashboardProfissional'
       }
+      else { window.location.href = '/dashbordCliente';}
+    }
+    else if (response.status === 401) {
+      window.location.href = '/unauthorized';
+      
+    } else {
+      const data = await response.json();
+      window.alert("Ops, algum problema ocorreu!");
+    }
   }
 
 } export default Login;
